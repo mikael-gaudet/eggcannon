b7dfe75fa2b7a2b44bd6d04806b2c4e50115bc6c
mikael-gaudet <mikael.gaudet@gmail.com>
2018-03-01 18:55:48 +0100

    PHP updates

--- send_post.php
+++ send_post.php
@@ -1,11 +1,16 @@
 <?php
+    $name = filter_input(INPUT_POST, 'name')
+    $name = filter_input(INPUT_POST, 'name')
+    $name = filter_input(INPUT_POST, 'name')
+    $name = filter_input(INPUT_POST, 'name')
     //setting up mysql details
-    $sql_server = 'sql208.byethost8.com';
-    $sql_user = 'b8_20819916';
-    $sql_pwd = '711webhosting';
-    $sql_db = 'b8_20819916_form';
+    $sql_server = 'fdb13.awardspace.net';
+    $sql_user = '2502818_sales';
+    $sql_pwd = 'database0';
+    $sql_db = '2502818_sales';
 
     //connecting to sql database
     $myslqi = new mysqli( $sql_server, $sql_user, $sql_pwd, $sql_db ) or die( $mysqli->error );
     $sql = "INSERT INTO sales (name,phone,address,pallets)"
+
 ?>
